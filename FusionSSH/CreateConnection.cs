f08c9f3be3561334fc0c0a1582b031c21ff9c50c
yungrixxxi <yungrixxxi@seznam.cz>
2024-07-21 16:37:51 +0200

    Added dialog window for Create ssh connection.

---
+++ FusionSSH/CreateConnection.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace FusionSSH
+{
+    public partial class CreateConnection : Form
+    {
+        public CreateConnection()
+        {
+            InitializeComponent();
+        }
+    }
+}
