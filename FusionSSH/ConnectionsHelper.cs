dc9820cf42fa70b96e88f646ca1863c44e590d4b
yungrixxxi <yungrixxxi@seznam.cz>
2024-07-21 17:01:00 +0200

    Added SSH connection structure

--- FusionSSH/ConnectionsHelper.cs
+++ FusionSSH/ConnectionsHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,40 @@ namespace FusionSSH
 {
     internal class ConnectionsHelper
     {
+        private string connectionDatabase;
+        
+
+        public ConnectionsHelper(string connectionDatabase)
+        {
+            this.connectionDatabase = connectionDatabase;
+        }
+
+        public void LoadDatabase()
+        {
+            if (!File.Exists(connectionDatabase))
+                CreateDatabase();
+
+            List<string> connectionsStrings = new List<string>();
+
+            string[] databaseRecords = GetDatabaseRecords();
+
+            if (databaseRecords != null)
+            {
+                foreach (var record in databaseRecords)
+                {
+
+                }
+            }
+        }
+
+        public void CreateDatabase()
+        {
+            File.WriteAllText(connectionDatabase, "");
+        }
+
+        public string[] GetDatabaseRecords()
+        {
+            return File.ReadAllLines(connectionDatabase);
+        }
     }
 }
