93e5a3478705436b9c4adbbb23a0435e4e4116ea
yungrixxxi <yungrixxxi@seznam.cz>
2024-07-21 16:30:23 +0200

    Added ParseValueFromKey(keyName, jsonString); for parsing the JSON values.

--- FusionSSH/JsonHelper.cs
+++ FusionSSH/JsonHelper.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace FusionSSH
 {
     internal class JsonHelper
     {
+        /// <summary>
+        /// Gets a value from key in JSON string.
+        /// </summary>
+        /// <param name="keyName">Name of the key</param>
+        /// <param name="jsonString">JSON string to be parsed</param>
+        /// <returns>Parsed value from JSON key as string</returns>
+        public static string ParseValueFromKey(string keyName, string jsonString)
+        {
+            try
+            {
+                JObject jsonObject = JObject.Parse(jsonString);
+                JToken value = jsonObject[keyName];
+
+                if (value != null)
+                {
+                    return value.ToString();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (JsonReaderException)
+            {
+                return null;
+            }
+        }
     }
 }
