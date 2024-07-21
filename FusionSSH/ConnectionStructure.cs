405348dbec03af9ba075597c1356efbad6bd24b4
yungrixxxi <yungrixxxi@seznam.cz>
2024-07-21 17:03:14 +0200

    Added important variables to the class.

--- FusionSSH/ConnectionStructure.cs
+++ FusionSSH/ConnectionStructure.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FusionSSH
+﻿namespace FusionSSH
 {
     internal class ConnectionStructure
     {
+        /// <summary>
+        /// Identifier for connection.
+        /// </summary>
+        public int ConnectionId;
+
+        /// <summary>
+        /// Name of the connections.
+        /// </summary>
+        public string ConnectionName;
+
+        /// <summary>
+        /// IP address of connection.
+        /// </summary>
+        public string ConnectionHost;
+
+        /// <summary>
+        /// Connection port
+        /// </summary>
+        public int ConnectionPort;
+
+        /// <summary>
+        /// Connection user.
+        /// </summary>
+        public string ConnectionUser;
+
+        /// <summary>
+        /// Password for user.
+        /// </summary>
+        public string ConnectionPass;
     }
 }
