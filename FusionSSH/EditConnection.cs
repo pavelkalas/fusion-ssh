cb941a9f2c4e02db281a5a4f8f8aad0df8be2189
yungrixxxi <yungrixxxi@seznam.cz>
2024-07-22 09:59:27 +0200

    Added logics for edit connection.

--- FusionSSH/EditConnection.cs
+++ FusionSSH/EditConnection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,19 +13,74 @@ namespace FusionSSH
 {
     public partial class EditConnection : Form
     {
-        public EditConnection()
+        private ConnectionsHelper connectionHelper;
+
+        private string connectionName;
+        private string jsonData;
+
+        public EditConnection(ConnectionsHelper connectionHelper, string connectionName)
         {
             InitializeComponent();
+
+            this.connectionHelper = connectionHelper;
+            this.connectionName = connectionName;
         }
 
         private void ConfirmEditBtn_Click(object sender, EventArgs e)
         {
+            // loads data from FORM.
+            string userName = (UserNameTxt.Text.Length > 0) ? UserNameTxt.Text : "root";
+            string connection = HostAndPortTxt.Text;
+            string passWord = PasswordTxt.Text;
+            string passWordAgain = PasswordAgainTxt.Text;
 
+            // create dummy variables
+            string ipAddress = null;
+            string port = null;
+
+            // check if user is putting port in HOST input, if yes, split string by ':'
+            if (connection.Contains(":"))
+            {
+                string[] connectionData = connection.Split(':');
+                ipAddress = connectionData[0].Trim();
+                port = connectionData[1].Trim();
+            }
+            else
+            {
+                port = "22";            // default SSH port 22
+                ipAddress = connection; // whole input as IP address
+            }
+
+            // checks if is password really correct
+            if (passWord != passWordAgain)
+            {
+                MessageBox.Show("Password and password again are not same!");
+                return;
+            }
+
+            // creating JSON object
+            var connectionString = new
+            {
+                ConnectionUser = userName,
+                ConnectionName = ipAddress,
+                ConnectionPort = port,
+                ConnectionHost = ipAddress,
+                ConnectionPass = DpapiHelper.Protect(passWord)
+            };
+
+            // generate JSON string from object above.
+            string jsonString = JsonConvert.SerializeObject(connectionString, Formatting.None);
+
+            connectionHelper.EditConnection(connectionName, jsonString);
+
+            // close this form
+            Hide();
         }
 
         private void CancelEditBtn_Click(object sender, EventArgs e)
         {
-
+            // close this form
+            Hide();
         }
     }
 }
