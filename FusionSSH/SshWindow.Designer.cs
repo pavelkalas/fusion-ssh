77b4b99ae295e17844ee5a27863d764f595a331c
yungrixxxi <yungrixxxi@seznam.cz>
2024-07-24 00:24:20 +0200

    Added package SSH.NET to project.

--- FusionSSH/SshWindow.Designer.cs
+++ FusionSSH/SshWindow.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TerminalOutputRich = new System.Windows.Forms.RichTextBox();
+            this.TerminalInputTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TerminalOutputRich
+            // 
+            this.TerminalOutputRich.Location = new System.Drawing.Point(12, 12);
+            this.TerminalOutputRich.Name = "TerminalOutputRich";
+            this.TerminalOutputRich.Size = new System.Drawing.Size(776, 398);
+            this.TerminalOutputRich.TabIndex = 0;
+            this.TerminalOutputRich.Text = "";
+            // 
+            // TerminalInputTxt
+            // 
+            this.TerminalInputTxt.Location = new System.Drawing.Point(12, 416);
+            this.TerminalInputTxt.Name = "TerminalInputTxt";
+            this.TerminalInputTxt.Size = new System.Drawing.Size(776, 22);
+            this.TerminalInputTxt.TabIndex = 1;
+            this.TerminalInputTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnCommandEntry);
+            // 
+            // SshWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TerminalInputTxt);
+            this.Controls.Add(this.TerminalOutputRich);
+            this.Name = "SshWindow";
             this.Text = "SshWindow";
+            this.Load += new System.EventHandler(this.OnFormLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox TerminalOutputRich;
+        private System.Windows.Forms.TextBox TerminalInputTxt;
     }
 }
